b90c659363c659ede7cc6a734f4c5e526e24c7e7
LazebnyiDanylo <daniillazebnuy124@gmail.com>
2025-06-03 15:56:37 +0300

    finish hw

---
+++ src/components/MovieModal/MovieModal.tsx
@@ -0,0 +1,52 @@
+import { KeyboardEvent, useEffect } from "react";
+import { Movie } from "../../types/movie";
+import css from "./MovieModal.module.css";
+
+interface MovieModalProps {
+  movie: Movie;
+  onClose(): void;
+}
+
+export default function MovieModal({ movie, onClose }: MovieModalProps) {
+  const backdropClose = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (event.key === `Escape`) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+      document.body.style.overflow = "";
+    };
+  });
+
+  return (
+    <div className={css.backdrop} role="dialog" aria-modal="true" onClick={backdropClose}>
+      <div className={css.modal}>
+        <button className={css.closeButton} aria-label="Close modal" onClick={onClose}>
+          &times;
+        </button>
+        <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} alt={movie.title} className={css.image} />
+        <div className={css.content}>
+          <h2>{movie.title}</h2>
+          <p>{movie.overview}</p>
+          <p>
+            <strong>Release Date:</strong> {movie.release_date}
+          </p>
+          <p>
+            <strong>Rating:</strong> {movie.vote_average}/10
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
